754f161dbd20925ba01faddb9d847b0f8c117461
Jonathan White <33507081+jonathan-white@users.noreply.github.com>
2020-07-26 18:19:48 -0400

    Fixed Collapsed ToolBar Title issue

--- app/src/main/java/com/example/android/warriornun/NunBioActivity.java
+++ app/src/main/java/com/example/android/warriornun/NunBioActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 
+import com.google.android.material.appbar.AppBarLayout;
 import com.google.android.material.appbar.CollapsingToolbarLayout;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
@@ -42,7 +43,26 @@ public class NunBioActivity extends AppCompatActivity {
         // Get the name of the nun
         String nameOfNun = getResources().getString(nunNameResourceId);
         // Set the toolbar title text to the nun's name
-        toolBarLayout.setTitle(nameOfNun);
+//        toolBarLayout.setTitle(nameOfNun);
+        AppBarLayout appBarLayout = findViewById(R.id.app_bar);
+        appBarLayout.addOnOffsetChangedListener(new AppBarLayout.OnOffsetChangedListener() {
+            boolean isShow = true;
+            int scrollRange = -1;
+
+            @Override
+            public void onOffsetChanged(AppBarLayout appBarLayout, int verticalOffset) {
+                if (scrollRange == -1) {
+                    scrollRange = appBarLayout.getTotalScrollRange();
+                }
+                if (scrollRange + verticalOffset == 0) {
+                    toolBarLayout.setTitle(nameOfNun);
+                    isShow = true;
+                } else if (isShow) {
+                    toolBarLayout.setTitle(" ");
+                    isShow = false;
+                }
+            }
+        });
 
         Drawable nunHeader = getResources().getDrawable(nun_header);
         toolBarLayout.setBackground(nunHeader);
